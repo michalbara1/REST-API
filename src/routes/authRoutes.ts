e5ef23e4c9574dae50831f49f438a5ada50bb14e
eden333333 <eden.neptune@gmail.com>
2024-12-27 12:47:35 +0200

    added toekn roues and controller

---
+++ src/routes/authRoutes.ts
@@ -0,0 +1,15 @@
+
+import express from "express";
+const router = express.Router();
+import authController from "../controllers/authController";
+
+router.post("/register", authController.register);
+
+router.post("/login", authController.login);
+
+router.post("/refresh", authController.refresh);
+
+router.post("/logout", authController.logout);
+
+
+export default router;
