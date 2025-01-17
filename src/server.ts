372ab843fbefa011c401a5279623a18f968b275e
Michal <michalbara1@gmail.com>
2025-01-17 14:40:31 +0200

    added enabling connection to forntend

--- src/server.ts
+++ src/server.ts
@@ -13,6 +13,12 @@ import swaggerUi from 'swagger-ui-express';
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Allow-Methods", "*");
+  next();
+});
 app.use("/posts", postsRoute);
 app.use("/comments", commentsRoute);
 app.use("/users", usersRoute);
